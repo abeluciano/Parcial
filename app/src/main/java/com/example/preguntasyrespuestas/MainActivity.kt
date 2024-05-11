f017a6ccd565ac6c5ef7ad98570b30274643f88e
abeluciano <aaragona@ulasalle.edu.pe>
2024-05-11 17:00:21 -0500

    Uniendo el main con el fragment welcome

--- app/src/main/java/com/example/preguntasyrespuestas/MainActivity.kt
+++ app/src/main/java/com/example/preguntasyrespuestas/MainActivity.kt
@@ -6,7 +6,9 @@ import androidx.appcompat.app.AppCompatActivity
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_main)
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, Welcome())
+            .commit()
     }
 }
